a23abc7563c334e669609e2a42da8adfa19a3e31
morgan4491 <m.morgan4491@gmail.com>
2024-10-02 22:18:38 -0400

    working on blog.js file still

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,12 +5,31 @@ const backBtn = document.querySelector('#back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
 
+function outputBlogList() {
+  const oldBlogArray = localStorage.getItem('blogs');
+  let blogArray;
+  if (oldBlogArray) {
+    blogArray = JSON.parse(oldBlogArray);
+  } else {
+    blogArray = [];
+  };
+  const blogOutput = document.querySelector('#blogOuput');
+
+  blogOutput.innerText = '';
+
+  if (!blogArray.length) {
+    blogOutput.innerText = `<li>No Blog posts yet...</li>`;
+  }
+
+  
+
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
 
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
 
 function renderBlogList() {
-    const data = 
+  const data = 
 }
 
 // TODO: Call the `renderBlogList` function
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,11 +3,6 @@ const inputForm = document.querySelector('#formInput'); // This variable 'inputF
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.querySelector('form');
-//     form.addEventListener('submit', handleFormSubmission);
-// });
-
 function handleFormSubmission(event) {
 
     const username = document.querySelector('#username').value;
@@ -41,16 +36,15 @@ function handleFormSubmission(event) {
         localStorage.setItem('blogs', JSON.stringify(blogArray));
 
         let redirectURL = '';
-
+    
         const redirectPage = function (url) {
             redirectURL = url;
             location.assign(url);
         };
-
-        redirectPage('blog.html');
-    }
-
-
+    
+        redirectPage('./blog.html');
+    };
+    
 };
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
